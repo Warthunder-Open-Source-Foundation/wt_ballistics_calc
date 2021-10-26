0dd02b0f949b26977eafd023966c98b2c7328168
flareflo <florianlipp17@gmail.com>
2021-10-26 20:04:53 +0200

    Further additions
    Took 1 hour 39 minutes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
 use std::f64::consts::{E, PI};
+
+use pad::PadStr;
 
 const MASS: f64 = 44.0;
 
@@ -10,11 +13,20 @@ const DIAM: f64 = 0.21;
 
 const CXK: f64 = 3.1;
 
-const RHO: f64 = 1.22;
+const RHO_0: f64 = 1.225;
+const RHO_500: f64 = 1.167;
+const RHO_1000: f64 = 1.11;
+const RHO_2000: f64 = 1.0;
+const RHO_4000: f64 = 0.819;
+const RHO_6000: f64 = 0.66;
+const RHO_8000: f64 = 0.525;
+const RHO_10000: f64 = 0.413;
 
 const TIMEFIRE0: f64 = 3.0;
 
-const THRUST0: f64 = 9500.0;
+const THRUST0: f64 = 95000.0;
+
+const ENDSPEED: f64 = 1000.0;
 
 const TIMESTEP: f64 = 0.1;
 
@@ -23,26 +35,26 @@ const SIMTIME: f64 = 21.0;
 fn main() {
 	let gravity = GRAVITY * 0.0;
 
-	let mut a: f64 = 0.0;
+	let mut a: f64;
 	let area = PI * (DIAM / 2.0).powi(2);
 
-	let mut drag_force: f64 = 0.0;
+	let mut drag_force: f64;
 
-	let mut velocity: f64 = 343.0;
+	let mut velocity: f64 = 367.0;
 
 	let mut distance: f64 = 0.0;
 
 	let launch_velocity = velocity;
 	let mut launch_distance = 0.0;
 
-	let target_velocity = 300.0;
-	let mut target_distance = 3500.0;
+	let target_velocity = 343.0;
+	let mut target_distance = 2000.0;
 
 	let mut max_v = 0.0;
 	let mut closest = f64::MAX;
 
-	for i in 0..((SIMTIME / TIMESTEP).round() as u32) {
-		drag_force = 0.5 * RHO * velocity.powi(2) * CXK.powi(-1) * area;
+	for i in 0..((SIMTIME / TIMESTEP).round().abs() as u64) {
+		drag_force = 0.5 * RHO * velocity.powi(2) * CXK * area;
 		if (i as f64 * TIMESTEP) < TIMEFIRE0 {
 			a = ((THRUST0 - drag_force) / MASS) - gravity;
 		} else {
@@ -52,14 +64,17 @@ fn main() {
 		target_distance += target_velocity * TIMESTEP;
 		launch_distance += launch_velocity * TIMESTEP;
 
-
 		velocity += a * TIMESTEP;
+		if velocity > ENDSPEED {
+			velocity = ENDSPEED;
+		}
+
 		distance += velocity * TIMESTEP;
 
-		// if target_distance < distance {
-		// 	println!("Splash at {}m! The target is {}m from the launch aircraft", target_distance, target_distance - launch_distance);
-		// 	break;
-		// }
+		if target_distance < distance {
+			println!("Splash! The target is {}m from the launch aircraft", (target_distance - launch_distance).round());
+			break;
+		}
 
 		if velocity > max_v {
 			max_v = velocity;
@@ -69,15 +84,23 @@ fn main() {
 			closest = target_distance - distance;
 		}
 
-		println!("m: {} t: {} a: {} v: {} d: {}", distance, target_distance, a, velocity, drag_force);
+		println!("i: {}  ld(m): {}  td(m): {}  dd(m): {}  am(ms2): {}  mv(m/s): {}  md(N): {}",
+				 i.to_string().pad_to_width(3),
+				 distance.round().to_string().pad_to_width(4),
+				 target_distance.round().to_string().pad_to_width(4),
+				 (target_distance - distance).round().to_string().pad_to_width(4),
+				 a.round().to_string().pad_to_width(4),
+				 velocity.round().to_string().pad_to_width(3),
+				 drag_force.round().to_string().pad_to_width(4)
+		);
 	}
-	println!("max v: {}", max_v);
-	println!("min missile - target: {}m", closest);
+	println!("max v: {}", max_v.round());
+	println!("delta target-missile: {}m", closest.round());
 }
 
 
 fn old_formula() {
-	let k = 0.5 * RHO * CXK * PI * (DIAM / 2.0).powi(2);
+	let k = 0.5 * RHO_0 * CXK * PI * (DIAM / 2.0).powi(2);
 
 	let mg = MASS * GRAVITY;
 
