307d0a5a9a224149fc5e25b5c9d59e331eed9778
flareflo <florianlipp17@gmail.com>
2021-11-03 14:35:49 +0100

    CLippy
    Took 26 minutes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use std::io;
-use std::thread::sleep;
-use std::time::Duration;
 use wt_missile_calc_lib::missiles::Missile;
 
 use calc::generate;
@@ -11,44 +9,43 @@ mod launch_parameters;
 mod calc;
 
 fn main() {
-	println!("{}", "Welcome to FlareFlo's missile-range calculator!");
-	println!("{}", "Please note that this tool is very WIP,\n\
+	println!("Welcome to FlareFlo's missile-range calculator!");
+	println!("Please note that this tool is very WIP,\n\
 					and currently only simulates:\n\
 					Sea Altitude\n\
 					Launch at mach 1");
 	println!("enable debug mode? y/n");
 
-	let mut debug = false;
-
 	let mut line = "".to_owned();
 	io::stdin()
 		.read_line(&mut line)
 		.expect("failed to read from stdin");
 
-	match line.trim() {
-		"y" => {debug = true}
-		_ => {}
-	}
+	let debug = match line.trim() {
+		"y" => {true}
+		_ => {false}
+	};
 
 	loop {
 		let missiles = Missile::new_from_generated(Some("./all.json"), None);
-		run_calc(missiles, debug)
+		run_calc(&missiles, debug);
 	}
 }
 
-fn run_calc(missiles: Vec<Missile>, debug: bool) {
-	println!("{}", "Enter which which missile to test (all lowercase)");
+#[allow(clippy::ptr_arg)]
+fn run_calc(missiles: &Vec<Missile>, debug: bool) {
+	println!("Enter which which missile to test (all lowercase)");
 
 	let mut line = "".to_owned();
 	io::stdin()
 		.read_line(&mut line)
 		.expect("failed to read from stdin");
 
-	if let None = Missile::select_by_name(&missiles, line.trim()) {
-		println!("{}", "Cannot find missile");
+	if Missile::select_by_name(missiles, line.trim()).is_none() {
+		println!("Cannot find missile");
 	}else {
-		let missile = Missile::select_by_name(&missiles, line.trim()).unwrap();
+		let missile = Missile::select_by_name(missiles, line.trim()).unwrap();
 
-		generate(missile, LaunchParameters::new_from_default_hor(), 0.1, debug);
+		generate(&missile, &LaunchParameters::new_from_default_hor(), 0.1, debug);
 	}
 }
