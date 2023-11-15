b83868e89a66f79c5fef36fcd1021c9b025eae6a
FlareFlo <mail@flareflo.dev>
2023-11-15 13:16:10 +0100

    Fix bug with confusing v with d, and added turning radius

--- src/lib/csv_export.rs
+++ src/lib/csv_export.rs
@@ -1,4 +1,5 @@
 use std::io::{Cursor, Write};
+use wt_datamine_extractor_lib::missile::missile::Missile;
 use zip::CompressionMethod;
 use zip::write::FileOptions;
 use crate::runner::LaunchResults;
@@ -6,9 +7,8 @@ use crate::runner::LaunchResults;
 
 
 impl LaunchResults {
-
 	// Returns zipped files containing results
-	pub fn as_csv(&self, plot: Option<&[u8]>) -> Option<Vec<u8>> {
+	pub fn as_csv(&self, plot: Option<&[u8]>, missile: Missile) -> Option<Vec<u8>> {
 		let out = Cursor::new(vec![]);
 		let mut zip = zip::ZipWriter::new(out);
 
@@ -31,14 +31,17 @@ impl LaunchResults {
 
 		zip.start_file("plot.csv", FileOptions::default().compression_method(CompressionMethod::Stored)).ok()?;
 
-		let mut csv = format!("acceleration;velocity;distance_traveled\n");
+		let mut csv = format!("acceleration;velocity;distance_traveled;turn_radius\n");
 
+		let turning_radius = |velocity: f64| {
+			velocity.powi(2) / missile.reqaccelmax
+		};
 		let profile = &self.profile;
 		let a = &profile.a;
-		let v = &profile.d;
+		let v = &profile.v;
 		let d = &profile.d;
 		for (a, v, d) in a.iter().zip(v.iter()).zip(d.iter()).map(|e|(e.0.0, e.0.1, e.1)) {
-			csv.push_str(&format!("{a};{v};{d}\n"));
+			csv.push_str(&format!("{a};{v};{d};{}\n", turning_radius(*v)));
 		}
 		zip.write_all(csv.as_bytes()).ok()?;
 
