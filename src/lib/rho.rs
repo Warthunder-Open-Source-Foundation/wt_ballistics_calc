aac2b6e1447b0a15ddd501fd5579a49bab59d6e0
FlareFlo <mail@flareflo.dev>
2024-07-03 22:07:11 +0200

    Adjust altitude formula

--- src/lib/rho.rs
+++ src/lib/rho.rs
@@ -1,32 +1,27 @@
 
-#[inline(always)]
-pub fn altitude_to_rho(altitude: u32) -> f64 {
-	return 3.0099_f64 - (0.868106 * ((0.0014659 * altitude as f64) + 7.88959).ln());
+// True to real life, but inaccurate for the game
+mod realistic {
+	use std::ops::Mul;
 
-	// 3.0099 - 0.868106 ln (0.0014659x + 7.88959)
-	// static CONSTANTS: [(u32, f64); 21] = [
-	// 	(0, 1.209),
-	// 	(500, 1.139),
-	// 	(1000, 1.072),
-	// 	(1500, 1.0083),
-	// 	(2000, 0.9478),
-	// 	(2500, 0.8903),
-	// 	(3000, 0.8356),
-	// 	(3500, 0.7837),
-	// 	(4000, 0.7344),
-	// 	(4500, 0.6877),
-	// 	(5000, 0.6434),
-	// 	(5500, 0.6014),
-	// 	(6000, 0.5617),
-	// 	(6500, 0.5241),
-	// 	(7000, 0.4886),
-	// 	(7500, 0.4551),
-	// 	(8000, 0.42345),
-	// 	(8500, 0.3936),
-	// 	(9000, 0.3655),
-	// 	(9500, 0.339),
-	// 	(10000, 0.3142),
-	// ];
+	/// Implements `https://www.grc.nasa.gov/www/k-12/airplane/atmosmet.html`
+	pub fn altitude_to_rho(altitude: u32) -> f64 {
+		alt_to_pressure(altitude) / (0.2869 * (18.0 + 273.1))
+	}
+
+	pub fn alt_to_pressure(altitude: u32) -> f64 {
+		let t = 15.04 - 0.00649.mul(altitude as f64);
+		let fraction = (t + 273.1) / 288.08;
+		let frac_exp = fraction.powf(5.256);
+
+		let pressure = 101.29 * frac_exp;
+		pressure
+	}
+}
+
+pub fn altitude_to_rho(altitude: u32) -> f64 {
+	let alt = altitude as f64 / 1000.0;
+	// y=−0.000036x³+0.002048x²−0.047574x+0.567771
+	-0.000036 * alt.powi(3) + 0.002048 * alt.powi(2) - 0.047574 * alt + 0.567771
 }
 
 #[cfg(test)]
@@ -36,5 +31,13 @@ mod tests {
 	#[test]
 	fn rho() {
 		println!("{}", altitude_to_rho(10000));
+	}
+
+	#[test]
+	fn test_parts() {
+		(0..=10)
+			.map(|e|e * 1000)
+			.map(|e|(e, altitude_to_rho(e)))
+			.for_each(|(a, e)|eprintln!("alt:{a}m {e:.3}"))
 	}
 }
